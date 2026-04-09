f5606e15baac6182e13690c2db34f3da2e928c3c
Aditya Tripathi <atri0048@student.monash.edu>
2026-04-10 00:20:00 +1000

    refactor: simplify button styles and remove unnecessary classes

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,16 @@ import Link from "next/link"
 
 const IndexPage = () => {
   return (
-    <div className="flex h-screen flex-col items-center justify-center gap-4 bg-foreground text-foreground">
-      <h1 className="text-4xl font-bold text-background text-white">
-        Choose a version
-      </h1>
+    <div className="flex h-screen flex-col items-center justify-center gap-4">
+      <h1 className="text-4xl font-bold">Choose a version</h1>
       <Link href="/v1">
-        <Button className="border-white" variant={"outline"}>
-          <span className="text-white">V1</span>
+        <Button variant={"outline"}>
+          <span className="">V1</span>
         </Button>
       </Link>
       <Link href="/v2">
-        <Button className="border-white" variant={"outline"}>
-          <span className="text-white">V2</span>
+        <Button variant={"outline"}>
+          <span className="">V2</span>
         </Button>
       </Link>
     </div>
